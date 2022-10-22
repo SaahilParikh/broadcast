451103ed5e4930c34ea3015ebeaa83fb3b74a673
Saahil Parikh <saahilparikh2000@gmail.com>
2022-10-22 02:25:31 -0700

    Revert "cleaning project to then re-commit with new git ignore template"
    This reverts commit 0ac10f3c26b29aa64eaf17f62420f2b83d9b47d3.

---
+++ src/backend/lambda_function_payload.js
@@ -0,0 +1,47 @@
+const AWS = require("aws-sdk");
+const dynamo = new AWS.DynamoDB.DocumentClient();
+
+exports.handler = async (event, context) => {
+  let body;
+  let statusCode = 200;
+  const headers = {
+	"Content-Type": "application/json"
+  };
+
+  let TableName = "broadcast-items";
+
+  try {
+	switch (event.routeKey) {
+  	case "GET /v1/items":
+    	body = await dynamo.scan({ TableName: TableName }).promise();
+    	break;
+  	case "PUT /v1/items":
+    	let requestJSON = JSON.parse(event.body);
+    	await dynamo
+      	.put({
+        	TableName: TableName,
+        	Item: {
+          	id: requestJSON.id,
+            user: requestJSON.user,
+            message: requestJSON.message,
+        	}
+      	})
+      	.promise();
+    	body = `Put item with id ${requestJSON.id} with message ${requestJSON.message} for user ${requestJSON.user}`;
+    	break;
+  	default:
+    	throw new Error(`Unsupported route: "${event.routeKey}"`);
+	}
+  } catch (err) {
+	statusCode = 400;
+	body = err.message;
+  } finally {
+	body = JSON.stringify(body);
+  }
+
+  return {
+	statusCode,
+	body,
+	headers
+  };
+};
